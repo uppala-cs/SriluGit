9956a934a4ff124a549ec06d9cdfc172574e994d
srilu <srilu@DESKTOP-K3O0VJD>
2023-10-23 14:20:27 -0700

    first commit

---
+++ GitSample/src/TestGit.java
@@ -0,0 +1,14 @@
+
+public class TestGit {
+	public static void main(String[] args)
+	{
+		System.out.print("Hello World");
+		System.out.print("Hello World again");
+		System.out.print("Hello World for the third time");
+		System.out.print("Hello World for the fourth time");
+		System.out.print("Hello World for the x time");
+
+		
+	}
+
+}
